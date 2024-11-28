78d26504172d8179fdb2627d43c465cf2a6d3d52
M26I <marija.ivanisevic267@gmail.com>
2024-11-28 14:55:16 +0000

    mysql connected

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'LittleLemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,11 +77,17 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',  # Replace with your actual database name
+        'USER': 'root',  # Replace with your MySQL username (default is 'root')
+        'PASSWORD': 'Espadrille10',  # Replace with your MySQL password (leave empty if no password)
+        'HOST': '127.0.0.1',  # Localhost for your local MySQL server
+        'PORT': '3306',  # Default MySQL port
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
